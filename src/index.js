185322c85f0bd94e3a03771b79180d522076a6b2
Alexey <alexey.anasov@yandex.by>
2024-05-19 20:09:26 +0300

    стилизация store + вывод карточек через API

--- src/index.js
+++ src/index.js
@@ -13,3 +13,60 @@ const changeActiveBtn = (event) => {
 buttons.forEach((button) => {
   button.addEventListener("click", changeActiveBtn);
 });
+
+const API_URL = "https://arrow-solid-water.glitch.me";
+
+const productList = document.querySelector(".store__list");
+
+const createProductCard = (product) => {
+  const productCard = document.createElement("li");
+  productCard.classList.add("store__item");
+  productCard.innerHTML = `
+    <article class="store__product product">
+      <img
+        class="product__img"
+        src="${API_URL}/${product.photoUrl}"
+        alt="${product.name}"
+        width="388"
+        height="261"
+      />
+
+      <h3 class="product__title">${product.name}</h3>
+
+      <p class="product__price">${product.price}&nbsp;₽</p>
+
+      <button class="product__btn-add-cart">Заказать</button>
+    </article>
+  `;
+
+  return productCard;
+};
+
+const renderProducts = (products) => {
+  productList.textContent = "";
+  products.forEach((product) => {
+    const productCard = createProductCard(product);
+
+    productList.append(productCard);
+  });
+};
+
+const fetchProductByCategory = async (category) => {
+  try {
+    const response = await fetch(
+      `${API_URL}/api/products/category/${category}`
+    );
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const products = await response.json();
+
+    renderProducts(products);
+  } catch (error) {
+    console.error(`Ошибка запроса товаров: ${error}`);
+  }
+};
+
+fetchProductByCategory("Домики");
