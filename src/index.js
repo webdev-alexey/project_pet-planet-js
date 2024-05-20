dc70e7b6b4d77b651c694a3164fe1b5aa802cc2f
Alexey <alexey.anasov@yandex.by>
2024-05-20 22:35:24 +0300

    стилизация карточек товаров + подключение API

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
+// const API_URL = "https://arrow-solid-water.glitch.me";
+const API_URL = "https://stormy-twisty-snowflake.glitch.me";
+
 const buttons = document.querySelectorAll(".store__category-button");
-
-const changeActiveBtn = (event) => {
-  const target = event.target;
-
-  buttons.forEach((button) => {
-    button.classList.remove("store__category-button_active");
-  });
-
-  target.classList.add("store__category-button_active");
-};
-
-buttons.forEach((button) => {
-  button.addEventListener("click", changeActiveBtn);
-});
-
-const API_URL = "https://arrow-solid-water.glitch.me";
-
 const productList = document.querySelector(".store__list");
+const cartButton = document.querySelector(".store__cart-button");
+const modalOverlay = document.querySelector(".modal-overlay");
+const cartItemsList = document.querySelector(".modal__cart-items");
+const modalCloseButton = document.querySelector(".modal-overlay__close-button");
 
-const createProductCard = (product) => {
+const createProductCard = ({ photoUrl, name, price }) => {
   const productCard = document.createElement("li");
   productCard.classList.add("store__item");
   productCard.innerHTML = `
     <article class="store__product product">
       <img
         class="product__img"
-        src="${API_URL}/${product.photoUrl}"
-        alt="${product.name}"
+        src="${API_URL}/${photoUrl}"
+        alt="${name}"
         width="388"
         height="261"
       />
 
-      <h3 class="product__title">${product.name}</h3>
+      <h3 class="product__title">${name}</h3>
 
-      <p class="product__price">${product.price}&nbsp;₽</p>
+      <p class="product__price">${price}&nbsp;₽</p>
 
       <button class="product__btn-add-cart">Заказать</button>
     </article>
@@ -69,4 +59,35 @@ const fetchProductByCategory = async (category) => {
   }
 };
 
-fetchProductByCategory("Домики");
+const changeCategory = ({ target }) => {
+  const category = target.textContent;
+
+  buttons.forEach((button) => {
+    button.classList.remove("store__category-button_active");
+  });
+
+  target.classList.add("store__category-button_active");
+  fetchProductByCategory(category);
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", changeCategory);
+
+  if (button.classList.contains("store__category-button_active")) {
+    fetchProductByCategory(button.textContent);
+  }
+  // fetchProductByCategory("Домики");
+});
+
+cartButton.addEventListener("click", () => {
+  modalOverlay.style.display = "flex";
+});
+
+modalOverlay.addEventListener("click", ({ target }) => {
+  if (
+    target === modalOverlay ||
+    target.closest(".modal-overlay__close-button")
+  ) {
+    modalOverlay.style.display = "none";
+  }
+});
